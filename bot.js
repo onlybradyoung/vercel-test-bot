69153ce20a9f7ef5751e1def7ea8cf32797126d2
onlybradyoung <bradyoung@mailfence.com>
2023-06-28 15:55:33 -0800

    update

--- bot.js
+++ bot.js
@@ -9,4 +9,4 @@ bot.on("message", async (ctx) => {
 
 // The free version of vercel has restrictions on quotas, which we need to enable in the configuration file vercel.json
 // webhookCallback will make sure that the correct middleware(listener) function is called
-export default webhookCallback(bot, "http");
+export default webhookCallback(bot, "https");
