3f7f53084eca7f0e2b4caec7f0bed89957869882
onlybradyoung <bradyoung@mailfence.com>
2023-06-28 08:40:18 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# [grammY](https://grammy.dev) on [Vercel](https://vercel.dev)
+
+You can deploy your bots to Vercel as a serverless function.
+
+Please run the `index.js` file in Vercel’s package command to set your webhook
+address.
---
+++ index.js
@@ -0,0 +1,8 @@
+import { Bot } from "grammy";
+import "dotenv/config";
+
+const bot = new Bot(process.env.BOT_TOKEN); // <-- Set your token in the vercel environment variable
+
+console.log(bot.token);
+
+// bot.api.setWebhook(process.env.WEBHOOK);
