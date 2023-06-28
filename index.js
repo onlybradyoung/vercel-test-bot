caf70195dfb68f0b6745528ff888b6d6c648a93a
onlybradyoung <bradyoung@mailfence.com>
2023-06-28 14:48:02 -0800

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-import { Bot } from "grammy";
-import "dotenv/config";
-
-const bot = new Bot(process.env.BOT_TOKEN); // <-- Set your token in the vercel environment variable
-
-console.log(bot.token);
-
-bot.api.setWebhook(process.env.WEBHOOK);
